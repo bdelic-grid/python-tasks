7e03fe592b307e3592ba8a81e4dff677bab41510
Bosko Delic <bdelic@griddynamics.com>
2025-01-21 11:43:57 +0100

    add 1-2.py

---
+++ 1-2.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    input_string = input("Please enter a list of integers separated using commas: ")
+
+    numbers = input_string.split(",")
+    numbers_set = set(numbers)
+    print(f"Removed duplicates: {numbers_set}")
+
+    numbers_tuple = tuple(numbers)
+    print(f"Tuple: {numbers_tuple}")
+
+    min = min(numbers_set)
+    max = max(numbers_set)
+    print(f"Min number is: {min} and max is: {max}")
+    
